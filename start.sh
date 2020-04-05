3c9625daa73152b462b8d9bb5120766bd4413031
Sean Ashton <sean@ashtech.one>
2020-04-05 15:03:22 +0800

    Cache old results if all machines are down

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --server-cert -b
+( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp
+#echo $ANYCONNECT_PASSWORD|openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --server-cert -b
 
-sleep 5
+#sleep 5
 
-iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -j ACCEPT
+#iptables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
+#iptables -A FORWARD -i eth0 -j ACCEPT
 
-route del -net 172.17.0.0 netmask 255.255.240.0  dev tun0
+#route del -net 172.17.0.0 netmask 255.255.240.0  dev tun0
 
+ping lab219-b06.cs.curtin.edu.au.
 cd BotFiles
 while true;do
 python3 CurtinLabBot.py
