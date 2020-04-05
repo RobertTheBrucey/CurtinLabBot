96931d987537cd8f8803e5e365e2a5184da08a3e
Sean Ashton <sean@ashtech.one>
2020-04-05 15:44:23 +0800

    More verbose output

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-#ping lab219-b06.cs.curtin.edu.au.
+for roo in {{218..221},232};
+do for col in {a..d};
+    do for row in {1..6};
+    do ping -c 1 lab$roo-${row}0$col.cs.curtin.edu.au.
+    done
+    done
+done
 cd BotFiles
 python3 CurtinLabBot.py
