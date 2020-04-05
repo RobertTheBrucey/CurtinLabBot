2dff81f099654881969698af775f7f85dd20bce2
Sean Ashton <sean@ashtech.one>
2020-04-05 13:59:17 +0800

    Dockerising

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp &
 ./BotFiles/start.sh
+( echo yes; echo $ANYCONNECT_PASSWORD ) | openconnect $ANYCONNECT_SERVER --user=$ANYCONNECT_USER --timestamp &
