78c7861424f32ed4c0da31d983f1ca58ff67f1e2
Sean Ashton <sean@ashtech.one>
2020-04-05 15:45:28 +0800

    More verbose output

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 for roo in {{218..221},232};
 do for col in {a..d};
     do for row in {1..6};
-    do ping -c 1 lab$roo-${row}0$col.cs.curtin.edu.au.
+    do 
+        ping -c 1 -w 1 lab$roo-${row}0$col.cs.curtin.edu.au.
+        nslookup lab$roo-${row}0$col.cs.curtin.edu.au.
     done
     done
 done
