0d585788efae76751d4c80bda64d66957ba16e4c
Sean Ashton <sean@ashtech.one>
2020-04-06 17:52:28 +0800

    Added persistent limit of 1 per channel

--- start.sh
+++ start.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-for roo in {{218..221},232};
-do for col in {a..d};
-    do for row in {1..6};
-    do 
-        #ping -c 1 -w 1 lab$roo-${col}0$row.cs.curtin.edu.au.
-        #nslookup lab$roo-${col}0$row.cs.curtin.edu.au.
-        :
-    done
-    done
-done
 cd BotFiles
 python3 CurtinLabBot.py
