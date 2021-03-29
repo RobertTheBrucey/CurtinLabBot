155918c7498c800dbf02eecbb7e03f5d0e17fcd9
Sean Ashton <sean@ashtech.one>
2021-03-29 18:05:18 +0800

    working on it

--- CurtinLabBot.py
+++ CurtinLabBot.py
@@ -9,9 +9,8 @@ bot = commands.Bot(command_prefix="^", case_insensitive=True)
 
 @bot.event
 async def on_ready():
-    # When connected, change presence and start twitch bot
     print(f"Logged in as {bot.user}")
 
 if __name__ == "__main__":
-    bot.add_cog(Labs(bot))
+    #bot.add_cog(Labs(bot))
     bot.run(getToken())
