da15f8153f9ef64f5e3dd3b7ae5b89d49fd8e13a
Sean Ashton <sean@ashtech.one>
2021-03-29 21:15:27 +0800

    Making more efficient

--- src/Labs.py
+++ src/Labs.py
@@ -291,9 +291,10 @@ class Labs(commands.Cog):
                         self.labs[host] = users
                         changed = True
                     if len(parts) > 4:
-                        if self.ips[host] != parts[4]:
-                            self.ips[host] = parts[4]
-                            changed = True
+                        if parts[4] not in self.ips.keys():
+                            if self.ips[host] != parts[4]:
+                                self.ips[host] = parts[4]
+                                changed = True
                     if (users>-1 and users < mini):
                         mini = users
                         mins = []
